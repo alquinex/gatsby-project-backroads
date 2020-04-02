68bbd238c7682862ee8520acf879562d7c223ace
alquinex <alquinex2010@hotmail.com>
2020-04-03 01:32:03 +0200

    first part of the project

---
+++ src/constants/services.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { FaWallet, FaTree, FaSocks } from "react-icons/fa"
+
+export default [
+  {
+    icon: <FaWallet />,
+    title: "saving money",
+    text:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium voluptatibus laudantium beatae earum! Nihil, ipsa.",
+  },
+  {
+    icon: <FaTree />,
+    title: "endless hiking",
+    text:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium voluptatibus laudantium beatae earum! Nihil, ipsa.",
+  },
+  {
+    icon: <FaSocks />,
+    title: "amazing comfort",
+    text:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium voluptatibus laudantium beatae earum! Nihil, ipsa.",
+  },
+]
