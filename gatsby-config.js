5e36e7ea5326148e29ebac2d01be2bc2483bd018
alquinex <alquinex2010@hotmail.com>
2020-04-10 18:56:46 +0200

    Env variables added

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: "BackRoads",
@@ -20,9 +24,9 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: `he49aaymcr3m`,
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
         // Learn about environment variables: https://gatsby.dev/env-vars
-        accessToken: "2ngzt2uMcJlKVovKbrUY-bd1-vi5zkHhC9A_d6TPAgo",
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
     `gatsby-transformer-sharp`,
